82cb4d7f4a4ffd2443004fd15153697b4498c101
Tea Muffin <wowcxy2008@126.com>
2018-04-18 14:28:39 +0800

    Update and rename qniu.js to qn.js

--- server/config/qn.js
+++ server/config/qn.js
@@ -3,21 +3,25 @@
  * @author tmuffin
  */
 
+// keys
 module.exports.accessKey = "KcH25vxJ8THYo9ExNp_-Kobr-TebdAZLss-JuLvD"
 module.exports.secretKey = "y82r6nTrPeQpjs5UXRdHlwePZh_9nFKIS-6gJFEn"
 
+// buckets
 module.exports.adminBucket = "blog-admin"
 module.exports.webBucket = "blog-web"
 module.exports.libsBucket = "blog-libs"
 module.exports.appBucket = "blog-app"
 module.exports.materialsBucket = "blog-materials"
 
+// domains
 module.exports.adminDomain = "p4i7ftd65.bkt.clouddn.com"
 module.exports.webDomain = "p4i7qqywc.bkt.clouddn.com"
 module.exports.libsDomain = "p4i8e3nz8.bkt.clouddn.com"
 module.exports.appDomain = "p4i8x9qcb.bkt.clouddn.com"
 module.exports.materialsDomain = "p4i8d5jo9.bkt.clouddn.com"
 
+// qn bukect
 module.exports.buckets = {
   "blog.materials": module.exports.materialsBucket,
   "blog.libs": module.exports.libsBucket,
@@ -28,6 +32,7 @@ module.exports.buckets = {
   "blog.robot.web": module.exports.webBucket,
 }
 
+// qn域
 module.exports.domains = {
   "blog.materials": module.exports.adminDomain,
   "blog.libs": module.exports.libsDomain,
@@ -38,15 +43,28 @@ module.exports.domains = {
   "blog.robot.web": module.exports.webDomain,
 }
 
+// 服务器文件路径映射
+module.exports.server = {
+  "blog.app": "",
+  "blog.web": "",
+  "blog.libs": "",
+  "blog.admin.web": "",
+  "blog.app.webchat": "",
+  "blog.robot.web": "",
+  "blog.materials": "",
+}
+
 /**
- * 获取项目bucket
+ * 获取项目 bucket
+ * @return {string} bucket
  */
 module.exports.getBucket = (project) => {
   return buckets[project]
 }
 
 /**
- * 获取项目domain
+ * 获取项目 domain
+ * @return {string} domain
  */
 module.exports.getDomain = (project) => {
   return domains[project]
