1442d67519c654a8715f4d06de207bd8d122de8d
Tea Muffin <wowcxy2008@126.com>
2018-04-18 14:46:24 +0800

    Update pull.js

--- server/deploy/pull.js
+++ server/deploy/pull.js
@@ -2,17 +2,16 @@
  * 一些配置
  * @author tmuffin
  */
-
-const shelljs = require('shelljs/global');
+const shelljs = require('shelljs/global')
 
 if (!which('git')) {
-  echo('Sorry, this script requires git');
-  exit(1);
+  echo('Sorry, this script requires git')
+  exit(1)
 }
 
 module.exports.exec = function () { 
   if (exec('git pull').code !== 0) {
-    echo('Error: pull failed');
-    exit(1);
+    echo('Error: pull failed')
+    exit(1)
   }
-};
+}
