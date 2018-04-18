578889b2d9e2f12b7143d138a5d456f518c3c6ff
Tea Muffin <wowcxy2008@126.com>
2018-04-18 14:45:49 +0800

    Update index.js

--- server/deploy/index.js
+++ server/deploy/index.js
@@ -3,12 +3,12 @@
  * @author tmuffin
  */
 
-const parse = require('./parse');
-const gitPull = require('./git/pull');
-const gitPush = require('./git/push');
-const build = require('./build');
-const publish = require('./publish');
-const restart = require('./restart');
+const build = require('./build')
+const parse = require('./parse')
+const gitPull = require('./git/pull')
+const gitPush = require('./git/push')
+const publish = require('./publish')
+const restart = require('./restart')
 
 /**
  * 部署
