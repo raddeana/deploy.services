ef9172ae4dd6f553b9531714a481cad7ab66e7e7
Tea Muffin <wowcxy2008@126.com>
2018-04-18 14:43:26 +0800

    Update restart.js

--- server/deploy/restart.js
+++ server/deploy/restart.js
@@ -3,7 +3,7 @@
  * @author tmuffin
  */
 
-const shelljs = require('shelljs/global');
+const shelljs = require('shelljs/global')
 
 /**
  * 重启项目
@@ -14,78 +14,86 @@ const restart = {
    * @return none
    */
   static: function (project) {
-    echo('Info: \t' + project + '\tis a app, skip');
+    echo('Info: \t' + project + '\tis a app, skip')
   },
+  
   /**
    * 微信 项目
    * @return none
    */
   webchat: function (project) {
-    echo('Info: \t' + project + '\tis a app, skip');
+    echo('Info: \t' + project + '\tis a app, skip')
   },
   /**
    * cordova 项目
    * @return none
    */
   cordova: function (project) {
-    echo('Info:\t' + project + '\tis a app, skip');
+    echo('Info:\t' + project + '\tis a app, skip')
   },
+  
   /**
    * java 重启项目
    * @return none
    */
   java: function (project) {
     if (exec('gradle build').code !== 0) {
-      echo('Warning: build failed');
-      echo('Info: restart\t' + project + '\tsuccess');
+      echo('Warning: build failed')
+      echo('Info: restart\t' + project + '\tsuccess')
     }
   },
+  
   /**
    * python 重启项目
    * @return none
    */
   python: function (project) {
     if (exec('uwsgi --reload app_uwsgi.pid').code !== 0) {
-      echo('Warning: restart failed');
-      echo('Warning: try to start the app');
+      echo('Warning: restart failed')
+      echo('Warning: try to start the app')
 
       if(exec('uwsgi --init app_uwsgi.ini').code !== 0) {
-        echo('Error: restart\t' + project + '\tfailed');
-        return;
+        echo('Error: restart\t' + project + '\tfailed')
+        return
       }
 
-      echo('Info: restart\t' + project + '\tsuccess');
+      echo('Info: restart\t' + project + '\tsuccess')
     }
   },
+  
   /**
    * node 重启项目
    * @return none
    */
   node: function (project) {
     if (exec('pm2 restart ' + project).code !== 0) {
-      echo('Warning: restart failed');
-      echo('Warning: try to start the app');
+      echo('Warning: restart failed')
+      echo('Warning: try to start the app')
 
       if(exec('pm2 start --name="' + project + '" npm -- start').code !== 0) {
-        echo('Error: restart\t' + project + '\tfailed');
-        return;
+        echo('Error: restart\t' + project + '\tfailed')
+        return
       }
 
-      echo('Info: restart\t' + project + '\tsuccess');
+      echo('Info: restart\t' + project + '\tsuccess')
     }
   },
-};
+}
 
+/**
+ * 执行
+ * @return none
+ */
 module.exports.exec = () => { 
   if (exec('pm2 restart ' + project).code !== 0) {
-    echo('Warning: restart failed');
-    echo('Warning: try to start the app');
+    echo('Warning: restart failed')
+    echo('Warning: try to start the app')
 
     if(exec('pm2 start --name="' + project + '" npm -- start').code !== 0) {
-      echo('Error: restart ' + project + ' failed');
-      return;
+      echo('Error: restart ' + project + ' failed')
+      return
     }
 
-    echo('info: restart ' + project + ' success');
+    echo('info: restart ' + project + ' success')
   }
-};
+}
