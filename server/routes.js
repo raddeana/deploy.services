584db990142717c13d1bbb61b08967941a35b4c4
Steudnera Taro <wowcxy2008@126.com>
2018-04-20 19:24:20 +0800

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -8,5 +8,19 @@
  * @param {object} express app
  */
 module.exports = (app) => {
-  
+  app.get('/', (req, res) => {
+    res.render('index.html')
+  })
+
+  app.get('/logs', (req, res) => {
+    getLogs(req, res)
+  })
+
+  app.post('/event/push', (req, res) => {
+    pushHandler(req, res)
+  })
+
+  app.post('/event/release', (req, res) => {
+    releaseHandler(req, res)
+  })
 }
