3791586a50f4cc70ed05db4ee6b98af253715cf2
Steudnera C. Koch <wowcxy2008@126.com>
2018-07-12 17:24:37 +0800

    Create git.js

---
+++ server/config/git.js
@@ -0,0 +1,12 @@
+/**
+ * 部署服务配置
+ * @author Steudnera
+ */
+
+export default {
+  dir: '~/deploy.services',
+  email: "wowcxy2008@126.com",
+  publish: "auto#publish",
+  ignore: "auto#ignore",
+  secret: "87656aca-9b3d-4f48-8db2-38ddcd4ad27f",
+}
