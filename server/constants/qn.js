b0eb6a4155431908e276c025c1c809acdc2d9010
Steudnera C. Koch <wowcxy2008@126.com>
2018-07-12 17:16:55 +0800

    Create qn.js

---
+++ server/constants/qn.js
@@ -0,0 +1,22 @@
+/**
+ * 七牛配置
+ * @author steudnera
+ */
+
+// keys
+export const accessKey = "KcH25vxJ8THYo9ExNp_-Kobr-TebdAZLss-JuLvD"
+export const secretKey = "y82r6nTrPeQpjs5UXRdHlwePZh_9nFKIS-6gJFEn"
+
+// buckets
+export const adminBucket = "blog.admin"
+export const webBucket = "blog.web"
+export const libsBucket = "blog.libs"
+export const appBucket = "blog.app"
+export const materialsBucket = "blog.materials"
+
+// domains
+export const adminDomain = "p4i7ftd65.bkt.clouddn.com"
+export const webDomain = "p4i7qqywc.bkt.clouddn.com"
+export const libsDomain = "p4i8e3nz8.bkt.clouddn.com"
+export const appDomain = "p4i8x9qcb.bkt.clouddn.com"
+export const materialsDomain = "p4i8d5jo9.bkt.clouddn.com"
