952a1eee118f1718a5453fe919888d0f1825e0d7
Steudnera C. Koch <wowcxy2008@126.com>
2018-07-16 12:26:47 +0800

    Update git.js

--- server/config/git.js
+++ server/config/git.js
@@ -2,11 +2,7 @@
  * 部署服务配置
  * @author Steudnera
  */
-
-export default {
-  dir: '~/deploy.services',
-  email: "wowcxy2008@126.com",
-  publish: "auto#publish",
-  ignore: "auto#ignore",
-  secret: "87656aca-9b3d-4f48-8db2-38ddcd4ad27f",
-}
+export const email = "wowcxy2008@126.com"
+export const auto_publish = "auto#publish"
+export const auto_ignore = "auto#ignore"
+export const secret = "87656aca-9b3d-4f48-8db2-38ddcd4ad27f"
