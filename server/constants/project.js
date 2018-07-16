1f0cc56b97dbff206757fb8699428b536c75edda
Steudnera C. Koch <wowcxy2008@126.com>
2018-07-16 12:23:15 +0800

    Update project.js

--- server/constants/project.js
+++ server/constants/project.js
@@ -1,50 +1,52 @@
 /**
- * 一些配置
- * @author steudnera
+ * 项目配置
+ * @author Steudnera
  */
 
 const git = 'https://github.com/steudnera'
 const path = '~/'
 
 // 项目开发语言配置
-export const type = {
+export const types = {
   blog: "java",
-  "blog.materials": "static",
-  "blog.libs": "static",
   "blog.web": "static",
+  "blog.admin": "node",
   "blog.admin.web": "static",
-  "blog.app": "cordova",
   "blog.robot": "python",
   "blog.robot.web": "static",
   "blog.apis": "node",
   "blog.services": "node",
+  "blog.games": "node",
+  "blog.portal": "node",
   "deploy.services": "node",
 }
 
 // 项目路径配置
-export const path = {
+export const paths = {
   blog: `${path}/blog`,
-  "blog.materials": `${path}/blog.images`,
-  "blog.libs": `${path}/blog.libs`,
   "blog.web": `${path}/blog.web`,
-  "blog.admin.web": `${path}/blog.admin.web`,
-  "blog.apis": `${path}/blog.apis`,
+  "blog.admin.web": `${path}/admin`,
+  "blog.admin.web": `${path}/admin/www`,
   "blog.robot": `${path}/blog.robot`,
   "blog.robot.web": `${path}/blog.robot.web`,
+  "blog.apis": `${path}/blog.apis`,
   "blog.services": `${path}/blog.services`,
+  "blog.games": "node",
+  "blog.portal": "node",
+  "deploy.services": "node",
 }
 
-// 项目路径配置
+// 仓库地址
 export const libraries = {
   blog: `${git}/blog`,
-  "blog.materials": `${git}/blog.materials`,
-  "blog.libs": `${git}/blog.libs`,
   "blog.web": `${git}/blog.web`,
+  "blog.admin": `${git}/blog.admin`,
   "blog.admin.web": `${git}/blog.admin.web`,
-  "blog.app": `${git}/blog.admin.app`,
-  "blog.apis": `${git}/blog.apis`,
   "blog.robot": `${git}/blog.robot`,
   "blog.robot.web": `${git}/blog.robot.web`,
+  "blog.apis": `${git}/blog.apis`,
   "blog.services": `${git}/blog.services`,
+  "blog.games": "node",
+  "blog.portal": "node",
   "deploy.services": `${git}/deploy.services`,
 }
