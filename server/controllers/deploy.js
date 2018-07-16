5275e887bdc98829d59aac8b296502a00d41de46
Steudnera C. Koch <wowcxy2008@126.com>
2018-07-16 12:30:05 +0800

    Update deploy.js

--- server/controllers/deploy.js
+++ server/controllers/deploy.js
@@ -1,44 +1,12 @@
 /**
  * 部署控制器
- * @author tmuffin
+ * @author Steudnera
  */
-
-function parse (res) {
-  const data = req.body
-  const commits = data.commits;
-
-  const deploy = false
-
-  let modified = []
-  let removed = []
-  let added = []
-  
-  commits.forEach(function (commit) {
-    if (commit.message !== base.publish && commit.message !== base.ignore) {
-      deploy = true
-    }
-
-    modified = modified.concat(commit.modified)
-    removed = removed.concat(commit.removed)
-    added = added.concat(commit.added)
-  })
-
-  const configure = {
-    deploy,
-    ref: data.ref,
-    project: data.repository.name,
-    master_branch: data.repository.master_branch,
-    modified,
-    removed,
-    added,
-  }
-
-  return configure
-}
 
 /**
  * 部署
+ * @param { object } 配置
  */
-module.exports = function () {
+export const deploy = () => {
   
 }
