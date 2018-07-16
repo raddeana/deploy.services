d519096ffe85e4eef640d5faf2786afd83f6dea9
Steudnera C. Koch <wowcxy2008@126.com>
2018-07-16 11:43:34 +0800

    Create user.js

---
+++ server/model/user.js
@@ -0,0 +1,44 @@
+/**
+ * 日志
+ * @author Steudnera
+ */
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// log schema
+const schema = new Schema({
+  username: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  password: {
+    type: String,
+    default: '',
+    trim: true
+  },
+})
+ 
+// 校验
+schema.path('username').required(true, 'username cannot be blank')
+schema.path('password').required(true, 'password cannot be blank')
+
+schema.statics = {
+  /**
+   * 重置密码
+   * @author Steudnera
+   */
+  resetPassword () {
+    
+  },
+
+  /**
+   * 登录
+   * @author Steudnera
+   */
+  login () {
+    
+  },
+}
+ 
+export default mongoose.model('log', schema)
