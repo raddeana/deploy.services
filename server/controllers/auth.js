3c4a145f793017c04279c66a00e1f019e486cb01
Steudnera C. Koch <wowcxy2008@126.com>
2018-07-18 10:27:05 +0800

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,3 +2,21 @@
  * 登录，忘记密码
  * @author Steudnera
  */
+
+/**
+ * 登录
+ * @param {object} 请求对象
+ * @param {object} 响应对象
+ */
+module.exports.login = () => {
+
+}
+
+/**
+ * 重设密码
+ * @param {object} 请求对象
+ * @param {object} 响应对象
+ */
+module.exports.resetpassword = () => {
+
+}
