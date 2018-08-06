3311d3a52a403a72e48a7a42568d472f041975fc
Steudnera C. Koch <wowcxy2008@126.com>
2018-08-06 20:03:42 +0800

    Update git.js

--- server/config/git.js
+++ server/config/git.js
@@ -3,6 +3,7 @@
  * @author Steudnera
  */
 module.exports.email = "wowcxy2008@126.com"
-module.exports.auto_publish = "auto#publish"
+module.exports.auto_push = "auto#push"
+module.exports.auto_release = "auto#release"
 module.exports.auto_ignore = "auto#ignore"
 module.exports.secret = "87656aca-9b3d-4f48-8db2-38ddcd4ad27f"
