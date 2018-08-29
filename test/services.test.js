2c573e61361da2f6fc5cd25bfc153f86ab2b9bc4
Discontinuity <wowcxy2008@126.com>
2018-08-29 21:34:01 +0800

    Update services.test.js

--- test/services.test.js
+++ test/services.test.js
@@ -2,8 +2,6 @@
  * 服务测试
  * @author Philip
  */
-const request = require("supertest")
-
 describe('deploy services', () => {
   it('should get log list', () => {
     assert.equal([1,2,3].indexOf(4), -1);
