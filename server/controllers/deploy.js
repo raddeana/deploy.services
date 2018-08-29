0c667436acb9607745d91e9e0403ec7e48c9317b
Discontinuity <wowcxy2008@126.com>
2018-08-29 21:44:30 +0800

    Update and rename hook.js to deploy.js

--- server/controllers/deploy.js
+++ server/controllers/deploy.js
@@ -2,9 +2,8 @@
  * 部署控制器
  * @author Philip
  */
-const parseRequest = require('../services/parse-request')
+const parseRequest = require('../services/parse-git-request')
 const catalog = require('../services/catalog')
-
 
 module.exports.push = (req, res) => {
   res.json({
