23632eeea313bb344b16a72427c389e331a976c9
Discontinuity <wowcxy2008@126.com>
2018-08-29 21:34:10 +0800

    Update apis.test.js

--- test/apis.test.js
+++ test/apis.test.js
@@ -2,8 +2,19 @@
  * 接口测试
  * @author Philip
  */
+const request = require("supertest")
 
-describe('deploy services', () => {
+describe('log', () => {
+  it('should get log list', () => {
+    assert.equal([1,2,3].indexOf(4), -1);
+  });
+  
+  it('should get log list', () => {
+    assert.equal([1,2,3].indexOf(4), -1);
+  });
+});
+
+describe('user', () => {
   it('should get log list', () => {
     assert.equal([1,2,3].indexOf(4), -1);
   });
