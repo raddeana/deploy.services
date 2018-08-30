535e5960ba57a0598c2f41571f983b1409abec1f
Discontinuity <wowcxy2008@126.com>
2018-08-30 11:23:39 +0800

    Create error-messages.js

---
+++ server/constants/error-messages.js
@@ -0,0 +1,15 @@
+/**
+ * 错误信息
+ * @author Philip
+ */
+
+module.exports.buildError = 'code build failed'
+module.exports.toProjectError = 'to project folder failed'
+module.exports.backDeployError = 'back deploy folder failed'
+module.exports.startError = 'start project failed'
+module.exports.restartError = 'restart project failed'
+module.exports.pullError = 'git pull failed'
+module.exports.pushError = 'git push failed'
+module.exports.parseGitRequestError = 'parse git request failed'
+module.exports.getConfigureError = 'get deploy configure failed'
+module.exports.getClientError = 'get qn client failed'
