4118386c3d1a7033abc321985c53a9deb61ab715
Discontinuity <wowcxy2008@126.com>
2018-08-30 12:39:12 +0800

    Update qn.js

--- server/services/qn.js
+++ server/services/qn.js
@@ -5,63 +5,62 @@
 const fs = require('fs')
 const qn = require('qn')
 
-module.exports = {
-  /**
-   * 获取项目发布配置
-   * @return none
-   */
-  getConfigure () {
-    let config = null
-    
-    try {
-      config = JSON.parse(fs.readFileSync('./deploy.config.json', 'utf8'))
-    } catch (e) {
-      throw (e)
-    }
-    
-    return config
-  },
+/**
+ * 遍历目录
+ * @param {string} 文件夹路径
+ * @param {function} 回调函数
+ * @return none
+ */
+const traverseDist = (path, callback) => {
+  const items = fs.readdirSync(path)
   
-  /**
-   * 获取qn客户端
-   * @return none
-   */
-  getClient () {
-    const configure = this.getConfigure()
-    const client = qn.create(configure)
+  items.forEach((item, index) => {
+    const info = fs.statSync(path + "/" + item)
     
-    return client
-  },
+    if (info.isDirectory()) {
+      readDirSync(path + "/" + item)
+    } else {
+      callback(file.path)
+    } 
+  })
+};
 
+/**
+ * 获取项目发布配置
+ * @return none
+ */
+const getConfigure = () => {
+  let config = null
   
-  /**
-   * 遍历目录
-   * @param {string} 文件夹路径
-   * @param {function} 回调函数
-   * @return none
-   */
-  traverseDist (path, callback) {
-  	const items = fs.readdirSync(path)
-    
-    items.forEach((item, index) => {
-      const info = fs.statSync(path + "/" + item)
-      
-      if (info.isDirectory()) {
-        readDirSync(path + "/" + item)
-      } else {
-        callback(file.path)
-      }	
-    })
-  },
+  try {
+    config = JSON.parse(fs.readFileSync('./deploy.config.json', 'utf8'))
+  } catch (e) {
+    throw (e)
+  }
   
+  return config
+};
+
+/**
+ * 获取qn客户端
+ * @return none
+ */
+const getClient = () => {
+  const qnConfig = getConfigure().qnConfig
+  const client = qn.create(qnConfig)
+  
+  return client
+};
+
+module.exports = {  
   /**
    * 删除
    * @return none
    */
   remove (distpath) {
-    const client = qn.create(config)
+    const client = getClient()
     
-    this.traverseDist(distpath, (filepath) => {
+    traverseDist(distpath, (filepath) => {
       client.delete(filepath, (err, result) => {
         if (!err) {
           console.log(result)
@@ -75,9 +74,9 @@ module.exports = {
    * @return none
    */
   upload (distpath) {
-    const client = qn.create(config)
+    const client = getClient()
     
-    this.traverseDist(distpath, (filepath) => {
+    traverseDist(distpath, (filepath) => {
       client.uploadFile(filepath, { key: filepath }, (err, result) => {
         if (!err) {
           console.info(result)
