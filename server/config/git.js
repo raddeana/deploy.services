2e37593a2934b780718276e322429851d60abbdc
Discontinuity <wowcxy2008@126.com>
2018-09-04 15:35:41 +0800

    Update git.js

--- server/config/git.js
+++ server/config/git.js
@@ -3,7 +3,7 @@
  * @author Philip
  */
 module.exports.email = "wowcxy2008@126.com"
-module.exports.auto_push = "auto#push"
-module.exports.auto_release = "auto#release"
-module.exports.auto_ignore = "auto#ignore"
+module.exports.autopush = /^autopush$/
+module.exports.autorelease = /^autorelease$/
+module.exports.autoignore = /^ignore$/
 module.exports.secret = "87656aca-9b3d-4f48-8db2-38ddcd4ad27f"
