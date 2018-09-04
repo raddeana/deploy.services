84ac789c42fa3c97ed28ab4929d25e6633bc9cb3
Discontinuity <wowcxy2008@126.com>
2018-09-04 18:19:41 +0800

    Create version.js

---
+++ server/controllers/version.js
@@ -0,0 +1,20 @@
+/**
+ * 版本控制
+ * @author chenxiangyu
+ */
+ 
+ /**
+  * 回滚
+  * @Controller
+  */
+ module.exports.rollBack = (res, req) => {
+ 
+ }
+ 
+  /**
+  * 删除版本
+  * @Controller
+  */
+ module.exports.delete = () => {
+ 
+ }
