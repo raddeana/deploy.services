9b2ae83b1a548c0c538f4ec2b9b70b4e9ceb59dd
Discontinuity <wowcxy2008@126.com>
2018-09-04 18:10:35 +0800

    Update cors.js

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -2,7 +2,6 @@
  * 跨域
  * @author Philip
  */
-
 module.exports = (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "X-Requested-With")
