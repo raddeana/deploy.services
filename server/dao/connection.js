e1e987a2796b97bb683a4f9d9634c1c6dfdf0500
Discontinuity <wowcxy2008@126.com>
2018-09-12 15:48:04 +0800

    Create connection.js

---
+++ server/dao/connection.js
@@ -0,0 +1,15 @@
+/**
+ * mongo 初始化
+ * @author Philip
+ */
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/deploy')
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+  console.info('mongoose connected')
+})
