04703c154f5f8a8294df6da44207c65b8c6f8744
Discontinuity <wowcxy2008@126.com>
2018-09-27 10:56:37 +0800

    Update and rename log.js to hook-record.js

---
+++ server/controllers/hook-record.js
@@ -0,0 +1,55 @@
+/**
+ * git钩子记录
+ * @author Philip
+ */
+const hookRecordDao = require('../dao/hook-record') 
+
+/**
+ * 记录查询
+ * @Controller
+ */
+module.exports.query = (res, req) => {
+  const params = req.params
+  const condition = {}
+  
+  Objects.keys(params).forEach((key) => {
+    const val = params[key]
+    
+    if (key !== 'pageIndex' || key !== 'pageSize') {
+      condition[key] = val
+    }
+  })
+  
+  const result = hookRecordDao.query(condition, params.pageIndex, params.pageSize)
+  
+  if (result.success) {
+    res.json(result.data)
+  } else {
+    res.send(result.code, { message: result.message })
+  }
+}
+
+/**
+ * 记录删除
+ * @Controller
+ */
+module.exports.remove = (res, req) => {
+  const params = req.params
+  const condition = {}
+  
+  Objects.keys(params).forEach((key) => {
+    const val = params[key]
+    
+    if (key !== 'pageIndex' || key !== 'pageSize') {
+      condition[key] = val
+    }
+  })
+  
+  const result = hookRecordDao.query(condition, params.pageIndex, params.pageSize)
+  
+  if (result.success) {
+    res.json(result.data)
+  } else {
+    res.send(result.code, { message: result.message })
+  }
+}
