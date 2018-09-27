38f0da8ea167ba37267e796b67cc062ed8c339f4
Discontinuity <wowcxy2008@126.com>
2018-09-27 11:07:44 +0800

    Update hook-data.js

--- server/dto/hook-data.js
+++ server/dto/hook-data.js
@@ -2,4 +2,56 @@
  * 解析过的 git hook 数据
  * @author chenxiangyu
  */
- 
+
+class HookData {
+  constructor (data) {
+    const commits = data.commits
+    const deploy = false
+
+    const modified = []
+    const removed = []
+    const added = []
+    const messages = []
+
+    commits.forEach((commit) => {
+      messages.push(commit.message)
+      modified = modified.concat(commit.modified)
+      removed = removed.concat(commit.removed)
+      added = added.concat(commit.added)
+    })
+
+    const _data = {
+      release: null,
+      ref: data.ref,
+      repository: data.repository.name,
+      master_branch: data.repository.master_branch,
+      default_branch: data.repository.default_branch,
+      messages,
+      modified,
+      removed,
+      added,
+    }
+
+    this.data = _data
+  }
+
+  /**
+   * 获取
+   * @return {object} 
+   */
+  get () {
+    return this.data
+  }
+  
+  /**
+   * 设置
+   * @param {string} 
+   * @param {string} 
+   * @return none
+   */
+  set (key, value) {
+    this.data[key] = value
+  }
+}
+
+module.exports = HookData
