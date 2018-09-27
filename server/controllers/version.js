076d9257f648569ae8fa2974b37fcf2894009d3b
Discontinuity <wowcxy2008@126.com>
2018-09-27 11:36:39 +0800

    Update version.js

--- server/controllers/version.js
+++ server/controllers/version.js
@@ -4,17 +4,17 @@
  */
  
  /**
-  * 回滚
+  * 版本回滚
   * @Controller
   */
- module.exports.rollBack = (res, req) => {
- 
+ module.exports.rollBack = (req, res) => {
+   const { projectId, version } = res.body
  }
  
-  /**
+ /**
   * 删除版本
   * @Controller
   */
- module.exports.delete = () => {
- 
+ module.exports.delete = (req, res) => {
+   const { projectId, version } = res.body;
  }
