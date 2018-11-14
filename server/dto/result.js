d10f342218a0ee493baa1350a4f1d44d1d092d18
Philip <wowcxy2008@126.com>
2018-11-14 10:14:31 +0800

    Update result.js

--- server/dto/result.js
+++ server/dto/result.js
@@ -1,4 +1,63 @@
 /**
  * 响应数据
- * @author chenxiangyu
+ * @author philip
  */
+class Result {
+  /**
+   * 构造函数
+   * @constructor
+   */
+  constructor (data) {
+    this.data = data;
+  }
+  
+  /**
+   * 设置数据
+   * @param {object} 数据
+   * @return none
+   */
+  setData (data) {
+    this.data = data;
+  }
+
+  
+  /**
+   * 获取数据
+   * @return {object} 数据
+   */
+  getData () {
+    return this.data;
+  }
+  
+  /**
+   * 分页
+   * @param {number} 分页size
+   * @param {number} 分页页码
+   * @return {object} 分页结果集
+   */
+  getPagination (pageSize, pageIndex) {
+    const data = this.data;
+    let listData = null;
+    
+    if (data instanceof Array) {
+      listData = Array;
+    } else {
+      listData = [data];
+    }
+    
+    return {
+      data: listData.slice((pageIndex - 1) * pageSize, pageIndex * pageSize),
+      total: listData.length,
+    };
+  }
+  
+  /**
+   * 错误信息
+   * @return { object } 错误对象
+   */
+  getErrorMsg (errorMsg) {
+    return {
+      errorMsg,
+    };
+  }
+}
